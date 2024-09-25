2898a0ee17ab4de7081353d98a76ceb7f25c3804
wahyuning06067 <sragikurikulum@gmail.com>
2024-09-25 19:46:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# wahyuning06067.github.id
+# wahyuning06067.github.io
+# wahyuning06067.github.io
+# wahyuning06067.github.io
+# wahyuning06067.github.io
+# wahyuning06067.github.io
---
+++ story_content/user.js
@@ -0,0 +1,167 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6glwVZIF9Y8":
+        Script1();
+        break;
+      case "5vcmOOKPTQv":
+        Script2();
+        break;
+      case "5dBkYI03Lrx":
+        Script3();
+        break;
+      case "6kVmEQPg3YV":
+        Script4();
+        break;
+      case "644mqgq4GXf":
+        Script5();
+        break;
+      case "6VFgR7XbAI6":
+        Script6();
+        break;
+      case "6fr8cLYZIrW":
+        Script7();
+        break;
+      case "5oSnwAeSrMt":
+        Script8();
+        break;
+      case "5r1MCDOxbn4":
+        Script9();
+        break;
+      case "5njNSaTWywN":
+        Script10();
+        break;
+      case "6EYnURuc6vy":
+        Script11();
+        break;
+      case "68ywxu8ZhSo":
+        Script12();
+        break;
+      case "69IQdS8z9pE":
+        Script13();
+        break;
+      case "5z0efVWv5cY":
+        Script14();
+        break;
+      case "5n1u3gMaL8j":
+        Script15();
+        break;
+      case "6XMAVwnWzRY":
+        Script16();
+        break;
+      case "65t3el55CU8":
+        Script17();
+        break;
+      case "6pkIN5l3czI":
+        Script18();
+        break;
+      case "5zCg00vmcIf":
+        Script19();
+        break;
+      case "5oPuF9VAeV2":
+        Script20();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.close();
+}
+
+function Script2()
+{
+  window.close();
+}
+
+function Script3()
+{
+  window.close();
+}
+
+function Script4()
+{
+  window.close();
+}
+
+function Script5()
+{
+  window.close();
+}
+
+function Script6()
+{
+  window.close();
+}
+
+function Script7()
+{
+  window.close();
+}
+
+function Script8()
+{
+  window.close();
+}
+
+function Script9()
+{
+  window.close();
+}
+
+function Script10()
+{
+  window.close();
+}
+
+function Script11()
+{
+  window.close();
+}
+
+function Script12()
+{
+  window.close();
+}
+
+function Script13()
+{
+  window.close();
+}
+
+function Script14()
+{
+  window.close();
+}
+
+function Script15()
+{
+  window.close();
+}
+
+function Script16()
+{
+  window.close();
+}
+
+function Script17()
+{
+  window.close();
+}
+
+function Script18()
+{
+  window.close();
+}
+
+function Script19()
+{
+  window.close();
+}
+
+function Script20()
+{
+  window.close();
+}
+
